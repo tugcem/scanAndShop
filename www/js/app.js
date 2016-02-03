fdf117407f5449f657399bfcf7f2fbc16256564b
Hatice Yalçın <haticeyalcin@Hatice-MacBook-Pro.local>
2016-02-03 17:26:02 +0200

    Put tabs to bottom for android app.

--- www/js/app.js
+++ www/js/app.js
@@ -82,4 +82,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
+})
+.config(function($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
 });
